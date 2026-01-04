a3940e046e773aad5c6ce8f4b3986e6e2ab4bc5a
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-01-04 16:53:39 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,33 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ui: ['@mui/material', '@emotion/react', '@emotion/styled'],
+          icons: ['lucide-react'],
+        }
+      }
+    }
+  }
+})
