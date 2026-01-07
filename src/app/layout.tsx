88185a3238c69b78d9c66005de3f9ce62943728a
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-01-07 19:03:56 -0400

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <MantineProvider>
+          {children}
+        </MantineProvider>
+      </body>
+    </html>
+  );
+}
