ad4029c4d20e49f113eb6428065b1f87251bebe2
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-01-07 19:43:06 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
+import type { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
