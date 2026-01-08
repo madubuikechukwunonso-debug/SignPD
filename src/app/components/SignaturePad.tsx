7b985f28ef2d430f70c2936f6d5b1cabea1059e2
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-01-07 20:28:55 -0400

    Update SignaturePad.tsx

--- src/app/components/SignaturePad.tsx
+++ src/app/components/SignaturePad.tsx
@@ -1,26 +1,33 @@
 "use client";
 
 import { forwardRef } from "react";
-import SignatureCanvas from "react-signature-canvas";
+import SignatureCanvasLib from "react-signature-canvas";
 
-/**
- * react-signature-canvas has incorrect TypeScript typings.
- * This wrapper fixes JSX + ref support safely.
- */
+/* ------------------------------------------------------------------ */
+/* Force react-signature-canvas into a valid JSX component             */
+/* ------------------------------------------------------------------ */
 
-export type SignaturePadHandle = SignatureCanvas;
-export type SignaturePadProps = {
-  canvasProps?: React.CanvasHTMLAttributes<HTMLCanvasElement>;
+// Hide the broken class typing from TypeScript completely
+const SignatureCanvas =
+  SignatureCanvasLib as unknown as React.FC<any>;
+
+/* ------------------------------------------------------------------ */
+/* Public handle type                                                  */
+/* ------------------------------------------------------------------ */
+
+export type SignaturePadHandle = {
+  clear: () => void;
+  toDataURL: (type?: string) => string;
+  isEmpty: () => boolean;
 };
 
-const SignaturePad = forwardRef<SignaturePadHandle, SignaturePadProps>(
-  ({ canvasProps }, ref) => {
-    return (
-      <SignatureCanvas
-        ref={ref as unknown as React.Ref<SignatureCanvas>}
-        canvasProps={canvasProps}
-      />
-    );
+/* ------------------------------------------------------------------ */
+/* Component                                                          */
+/* ------------------------------------------------------------------ */
+
+const SignaturePad = forwardRef<SignaturePadHandle, any>(
+  (props, ref) => {
+    return <SignatureCanvas ref={ref} {...props} />;
   }
 );
 
