24cb95f904cfcd0d13027e773d84568fc3842337
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-01-30 16:43:59 -0400

    Create useImages.ts

---
+++ app/hooks/useImages.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { getImages, saveImages } from "@/app/lib/idb";
+
+export type PdfImage = {
+  id: string;
+  page: number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  embedKey: string; // key in pdf-lib embed map
+  fileName?: string;
+};
+
+export function useImages() {
+  const [images, setImages] = useState<PdfImage[]>([]);
+
+  useEffect(() => {
+    getImages().then(setImages);
+  }, []);
+
+  const addImage = (img: Omit<PdfImage, "id">) => {
+    const next = [...images, { ...img, id: crypto.randomUUID() }];
+    setImages(next);
+    saveImages(next);
+  };
+
+  const deleteImage = (id: string) => {
+    const next = images.filter((i) => i.id !== id);
+    setImages(next);
+    saveImages(next);
+  };
+
+  return { images, addImage, deleteImage };
+}
