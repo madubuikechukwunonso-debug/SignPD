66d12799eea72c162e4265463af5d6f380a1aa5c
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-01-30 15:42:16 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+export const metadata = {
+  title: "SignPD",
+  description: "Edit, sign, highlight & rearrange your PDFs in the browser—no uploads to any server.",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <DndProvider backend={HTML5Backend}>{children}</DndProvider>
+      </body>
+    </html>
+  );
+}
