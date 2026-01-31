ac3ad4e8309159b98c13bcda02cabf69946d3832
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-01-30 23:16:48 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,4 @@
-import { Metadata } from "next";
 import Providers from "@/app/components/Providers";
-
-export const metadata: Metadata = {
-  title: "SignPD",
-  description: "Edit, sign, highlight & rearrange your PDFs in the browser—no uploads to any server.",
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
