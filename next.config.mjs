e2bfbd87d8c5603333bcea4c62a9cd8e53a74652
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-01-30 22:36:02 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,23 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
-  // allow SVG imports for stamp icons
-  webpack(config) {
+  webpack(config, { isServer }) {
+    // keep SVG rule
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"],
     });
+
+    // force react-dnd & framer-motion to stay out of server bundle
+    if (isServer) {
+      config.externals.push(
+        "react-dnd",
+        "react-dnd-html5-backend",
+        "framer-motion"
+      );
+    }
+
     return config;
   },
 };
