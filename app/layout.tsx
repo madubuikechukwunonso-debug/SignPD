79356c5e2b1df960301bac67c4413eeb194197dc
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-01-31 13:58:07 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import "./globals.css";
 import { Metadata } from "next";
 import Providers from "@/app/components/Providers";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${inter.className} antialiased`}>
         <Providers>{children}</Providers>
       </body>
     </html>
