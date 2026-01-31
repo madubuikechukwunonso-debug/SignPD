e8c1c596805030564d71bc20a7754648ccbdfd93
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-01-31 13:33:44 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Metadata } from "next";
 import Providers from "@/app/components/Providers";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
