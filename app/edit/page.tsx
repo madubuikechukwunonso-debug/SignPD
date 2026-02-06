724f23e6ab26c2085407efde5b5af87693837fa3
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-02-05 20:26:21 -0400

    Update page.tsx

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -32,7 +32,7 @@ export default function EditPage() {
 
   // Worker src (your local file)
   useEffect(() => {
-    pdfjs.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.min.js';
+    pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
   }, []);
 
   // Initialize page order on load
